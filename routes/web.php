e01b33d642929d6751ae90a7c5345ee94d06bcc3
yehuifang <630458731@qq.com>
2019-08-28 09:05:27 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
